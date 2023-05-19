8e2edca72a751009cc5e415eca35e37f212cd66b
nhatzi <nickohatz@gmail.com>
2023-05-19 10:58:39 -0400

    split done, now to start making the matrix squares

--- encrypt.py
+++ encrypt.py
@@ -1,5 +1,7 @@
 import sys
-tbEncrypt = "ORANGEE"
+msg = "ATTACK AT DAWN"
+tbEncrypt = msg.replace(" ", "")
 if(len(tbEncrypt)%2 != 0):
 	tbEncrypt += "Q"
-print([(tbEncrypt[i:i+2]) for i in range(0, len(tbEncrypt), 2)])
+splitted = ([(tbEncrypt[i:i+2]) for i in range(0, len(tbEncrypt), 2)])
+print(splitted)
