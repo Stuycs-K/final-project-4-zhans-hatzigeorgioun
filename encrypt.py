2f54709ae39ba75249807bcf83ee504ac6a3a0d5
zhansean <zhansean@gmail.com>
2023-05-22 10:28:11 -0400

    added generate key function

--- encrypt.py
+++ encrypt.py
@@ -5,3 +5,30 @@ if(len(tbEncrypt)%2 != 0):
 	tbEncrypt += "Q"
 splitted = ([(tbEncrypt[i:i+2]) for i in range(0, len(tbEncrypt), 2)])
 print(splitted)
+
+def generate_key_square(key):
+    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+    key = key.upper().replace("J", "I")
+    key_square = [[None] * 5 for _ in range(5)]
+
+    used_letters = set()
+    row, col = 0, 0
+    for letter in key:
+        if letter not in used_letters:
+            key_square[row][col] = letter
+            used_letters.add(letter)
+            col += 1
+            if col == 5:
+                col = 0
+                row += 1
+
+    for letter in alphabet:
+        if letter != "J" and letter not in used_letters:
+            key_square[row][col] = letter
+            used_letters.add(letter)
+            col += 1
+            if col == 5:
+                col = 0
+                row += 1
+
+    return key_square
