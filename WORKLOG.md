d9c40aae73fd936d05a2083b2ba06bf58424f924
zhansean <zhansean@gmail.com>
2023-05-24 21:38:10 -0400

    function adds to encrypt and worklog updated

--- WORKLOG.md
+++ WORKLOG.md
@@ -40,6 +40,9 @@ Continue research, looked into case where messages contain an odd number of char
 
 Wrote generate key function, added in user input for key, and displayed output of key for demonstration purposes
 
+### May 24, 202
+Added find_position function and encrypt function, redid splitting so that spaces were accounted for in alphabet under key function, and added printing of encrypted output. Needs to be tested to make sure it works correctly.
+
 
 ## References and Resources
 
--- encrypt.py
+++ encrypt.py
@@ -1,18 +1,10 @@
 import sys
-msg = input("Enter the message you want to encrypt: ")
+message = input("Enter the message you want to encrypt: ")
 key1 = input("Enter your first encryption key: ")
 key2 = input("Enter your second encryption key: ")
-def splitter (msg):
-    tbEncrypt = msg.replace(" ", "").upper()
-    if(len(tbEncrypt)%2 != 0):
-    	tbEncrypt += "Q"
-    splitted = ([(tbEncrypt[i:i+2]) for i in range(0, len(tbEncrypt), 2)])
-    return splitted
-
-print(splitter(msg))
 
 def generate_key_square(key):
-    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ "
     key = key.upper().replace("J", "I")
     key_square = [[None] * 5 for _ in range(5)]
 
@@ -26,6 +18,8 @@ def generate_key_square(key):
             if col == 5:
                 col = 0
                 row += 1
+                if row == 5:
+                    break
 
     for letter in alphabet:
         if letter != "J" and letter not in used_letters:
@@ -35,18 +29,46 @@ def generate_key_square(key):
             if col == 5:
                 col = 0
                 row += 1
+                if row == 5:
+                    break
 
     return key_square
 
-generate_key_square(key1)
-generate_key_square(key2)
-#print out the key to show whats happening
-print(generate_key_square(key1))
-print(generate_key_square(key2))
+def find_position(square, letter):
+    for row in range(5):
+        for col in range(5):
+            if square[row][col] == letter:
+                return row, col
 
-default_key_square = ['A', 'B', 'C', 'D', 'E', 
-					  'F', 'G', 'H', 'I', 'J', 
-					  'K', 'L', 'M', 'N', 'O', 
-					  'P', 'R', 'S', 'T', 'U', 
-  					  'V', 'W', 'X', 'Y', 'Z']
-         
+def four_square_encrypt(message, key_square1, key_square2):
+    message = message.upper().replace("J", "I")
+    cipher_text = ""
+    for i in range(0, len(message), 2):
+        letter1 = message[i]
+        letter2 = message[i + 1] if i + 1 < len(message) else "X"
+
+        if letter1 == " ":
+            cipher_text += " "
+            continue
+
+        if letter2 == " ":
+            cipher_text += " "
+            continue
+
+        row1, col1 = find_position(key_square1, letter1)
+        row2, col2 = find_position(key_square2, letter2)
+
+        cipher_text += key_square2[row1][col2]
+        cipher_text += key_square1[row2][col1]
+
+    return cipher_text
+
+key_square1 = generate_key_square(key1)
+key_square2 = generate_key_square(key2)
+
+# Print out the key squares to show what's happening
+print(key_square1)
+print(key_square2)
+
+encrypted = four_square_encrypt(message, key_square1, key_square2)
+print(encrypted)
